8372ed9de8f14e1ba8060311f611f085b49129e2
Talisson Avila <talisson_avila@hotmail.com>
2022-10-13 10:22:28 -0300

    edit code
    consertado o código do contador de inversão força bruta

--- contadoriversaobruto.py
+++ contadoriversaobruto.py
@@ -2,12 +2,12 @@
 
 def conta_inversao_bruto(vetor):
     contador = 0
-    for i in range(1, len(vetor) - 1):
+    for i in range(len(vetor) - 1):
         for j in range(i + 1, len(vetor)):
             if vetor[i] > vetor[j]:
                 contador += 1
     return contador
 
 
-vetorTeste = [10, 3, 7, 13, 9, 1]
-print(f'O numero de inversões é de: {conta_inversao_bruto(vetorTeste)}')
+vetor_teste = [10, 3, 7, 13, 9, 1]
+print(f'O numero de inversões é de: {conta_inversao_bruto(vetor_teste)}')
